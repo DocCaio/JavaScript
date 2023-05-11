b9a477f0b75b409b1a6f834b0fdaca2e1233a6ae
Caio <108128119+DocCaio@users.noreply.github.com>
2023-05-11 10:14:46 -0300

    update

--- logica-desenho/formas.js
+++ logica-desenho/formas.js
@@ -1,15 +1,26 @@
-function desenhaQuadradoVerde(x , y) {
+function desenhaQuadrado(x, y, cor) {
 
-    var tela = document.querySelector('canvas');
-    var pincel = tela.getContext('2d');
+    let tela = document.querySelector('canvas');
+    let pincel = tela.getContext('2d');
 
-    pincel.fillStyle = 'green';
+    pincel.fillStyle = cor;
     pincel.fillRect(x, y, 50, 50);
     pincel.strokeStyle = 'black';
     pincel.strokeRect(x, y, 50, 50);
 
 }
 
-desenhaQuadradoVerde();
-desenhaQuadradoVerde();
-desenhaQuadradoVerde();
+
+
+/*
+var x = 0;
+
+while(x < 600) {
+
+    desenhaQuadrado(x, 0, 'green');
+x = x + 50;
+}
+*/
+for(var x = 0; x < 600; x = x + 50) {
+desenhaQuadrado(x, 0, 'green');
+}
