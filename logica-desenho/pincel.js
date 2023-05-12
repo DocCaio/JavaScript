4026bca2c1c03cfea4341379099df48e540e4a9a
Caio <108128119+DocCaio@users.noreply.github.com>
2023-05-12 08:54:06 -0300

    update

--- logica-desenho/pincel.js
+++ logica-desenho/pincel.js
@@ -4,6 +4,17 @@ let pincel = tela.getContext('2d');
 pincel.fillStyle = 'grey';
 pincel.fillRect(0 , 0 , 600 ,400);
 
-function exibeAlerta() {
-    alert('Cliquei')
+function desenhaCirculo(evento) {
+
+    let x = evento.pagex - tela.offsetLeft;
+    let y = evento.pagey - tela.offsetTop;
+    pincel.fillStyle = 'blue';
+    pincel.beginPath();
+    pincel.arc(x , y , 10, 0 ,2 * 3.14);
+    pincel.fill(); 
+
+   console.log(x + ',' + y);
+   
 }
+
+tela.onclick = desenhaCirculo;
