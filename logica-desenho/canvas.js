1d184f3ad26dc9a98a5142f4d889412e649b26fc
Caio <108128119+DocCaio@users.noreply.github.com>
2023-05-15 08:35:18 -0300

    update

--- logica-desenho/canvas.js
+++ logica-desenho/canvas.js
@@ -1,5 +1,5 @@
-const tela = document.querySelector('canvas');
-    const pincel = tela.getContext('2d');
+var tela = document.querySelector('canvas');
+   var pincel = tela.getContext('2d');
 
     pincel.fillStyle = 'lightgrey';
     pincel.fillRect(0, 0, 600, 400);
--- logica-desenho/pincel.js
+++ logica-desenho/pincel.js
@@ -1,13 +1,13 @@
-let tela = document.querySelector('canvas');
-let pincel = tela.getContext('2d');
+var tela = document.querySelector('canvas');
+var pincel = tela.getContext('2d');
 
 pincel.fillStyle = 'grey';
 pincel.fillRect(0 , 0 , 600 ,400);
 
 function desenhaCirculo(evento) {
 
-    let x = evento.pagex - tela.offsetLeft;
-    let y = evento.pagey - tela.offsetTop;
+    var x = evento.pagex - tela.offsetLeft;
+    var y = evento.pagey - tela.offsetTop;
     pincel.fillStyle = 'blue';
     pincel.beginPath();
     pincel.arc(x , y , 10, 0 ,2 * 3.14);
