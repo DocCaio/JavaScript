5c48f8af1fd815a6b635a0e18ed0544377e75a0a
Caio <108128119+DocCaio@users.noreply.github.com>
2023-05-19 10:31:37 -0300

    update

--- introducao-javascript-2/js/principal.js
+++ introducao-javascript-2/js/principal.js
@@ -1,3 +1,33 @@
 let titulo = document.querySelector(".titulo");
 
-titulo.textContent = "Aparecida Nutricionista"
+titulo.textContent = "Aparecida Nutricionista";
+
+let paciente = document.querySelector("#primeiro-paciente");
+
+let tdPeso = paciente.querySelector(".info-peso");
+let peso = tdPeso.textContent;
+
+let tdAltura = paciente.querySelector(".info-altura");
+let altura = tdAltura.textContent;
+
+
+let tdImc = paciente.querySelector(".info-imc");
+
+
+ 
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
