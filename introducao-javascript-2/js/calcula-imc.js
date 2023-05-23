3d7ced1ae5733a0ed488a98d0e9a88c81923c22e
Caio <108128119+DocCaio@users.noreply.github.com>
2023-05-23 10:44:32 -0300

    update

--- introducao-javascript-2/js/calcula-imc.js
+++ introducao-javascript-2/js/calcula-imc.js
@@ -24,10 +24,18 @@ if (altura <= 0 || altura >= 3.00) {
     console.log("Altura inválida!");
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida";
+    paciente.classList.add("paciente-invalido");
 }
 
 if (pesoEhValido && alturaEhValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc.toFixed(2);
+   let imc = calculaImc(peso,altura);
+    tdImc.textContent = imc;
 }
 
+function calculaImc(peso,altura){
+    let imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
