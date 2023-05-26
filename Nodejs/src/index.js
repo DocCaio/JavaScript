4099b3fa4badd10056e452b211755e50bc968445
Caio <108128119+DocCaio@users.noreply.github.com>
2023-05-26 09:30:36 -0300

    update

---
+++ Nodejs/src/index.js
@@ -0,0 +1,26 @@
+function hello() {
+  console.log("Hello"); 
+   
+
+    } 
+
+    function helloTwo() {
+        console.log("Hello2");
+        hello();
+    }
+
+
+helloTwo();
+
+
+// if(true) {
+       // let nome = "João"; //STRING
+       // const numero = 1; //number
+      //  const boolean = false; //boolean
+       // const array =["1", 2, true, {}]; //boolean
+       // const objeto ={nome: "Gol", ano: 1987, km: "18.000 km" , primeiroDono : false}; //object
+        //console.log(`Meu nome é ${nome}`);
+
+           // console.log(objeto.km);  
+   
+//}
