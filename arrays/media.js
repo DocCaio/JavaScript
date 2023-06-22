a864c682bdc46df48d00253529e944d177e6c26b
Caio <108128119+DocCaio@users.noreply.github.com>
2023-06-22 13:24:49 -0300

    update

--- arrays/media.js
+++ arrays/media.js
@@ -8,6 +8,6 @@
 
 const notas = [10, 6.5, 8 ,7.5];
 
-const media = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4]) /4;
+const media = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4]) / notas.length;
 
 console.log(media);
