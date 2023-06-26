577f207db111bef59d1862f2903676d9ef1cc453
Caio <108128119+DocCaio@users.noreply.github.com>
2023-06-26 08:50:55 -0300

    up

---
+++ arrays/map-nomes.js
@@ -0,0 +1,10 @@
+const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+
+//const nomesPadronizados = nomes.map((nome) => {
+   // return nome.toUpperCase();
+//});
+
+
+const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesPadronizados);
