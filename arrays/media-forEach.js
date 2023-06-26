923f2aeb2b0c561c2ebfa084040a08d30f4eaaf2
Caio <108128119+DocCaio@users.noreply.github.com>
2023-06-26 08:27:03 -0300

    up

--- arrays/media-forEach.js
+++ arrays/media-forEach.js
@@ -1,5 +1,11 @@
 const notas = [10 , 6.5 , 8 ,7.5];
 
-notas.forEach(function () {
-  console.log("oi");
+let somasDasNotas = 0;
+
+notas.forEach(function (nota) {
+  somasDasNotas += nota;
 });
+
+const media = somasDasNotas / notas.length;
+
+console.log(`A média das notas é ${media}.`)
