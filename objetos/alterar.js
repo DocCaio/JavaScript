669a321d4a965a530f54b0a3a19584c32c4fbe01
Caio <108128119+DocCaio@users.noreply.github.com>
2023-06-27 07:50:59 -0300

    up

---
+++ objetos/alterar.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: "Luma",
+    prosissao: "Engenheira",
+
+};
+
+console.log(pessoa.nome);
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = "11 222333444";
+
+console.log(pessoa.telefone);
