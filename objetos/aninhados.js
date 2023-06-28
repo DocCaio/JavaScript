40e6617f81bf6df7076a12a533e057c0eb715a64
Caio <108128119+DocCaio@users.noreply.github.com>
2023-06-28 08:43:32 -0300

    up

---
+++ objetos/aninhados.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome: "Joao",
+    idade:24,
+    email:"Joao@forma.com",
+    telefone:["11555550" , "1144440"],
+    
+};
+
+cliente.endereco = {
+    rua: "R. Joseph Clienber",
+    numero: 1337,
+    apartamento:true,
+    complemento: "ap 934",
+};
+
+console.log(cliente);
---
+++ objetos/tipos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Joao",
+    idade:24,
+    email:"Joao@forma.com",
+    telefone:["11555550" , "1144440"],
+    
+};
+
+console.log(cliente.telefone);
