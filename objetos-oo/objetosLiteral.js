d56f6f9873f69d5682e41a5df4b641ef0621da6a
Caio <108128119+DocCaio@users.noreply.github.com>
2023-07-03 08:48:26 -0300

    up

--- objetos-oo/objetosLiteral.js
+++ objetos-oo/objetosLiteral.js
@@ -1,5 +1,5 @@
 const user = {
-    nome: "Juliana",
+    nome: "estudante",
     email: "c@c.com",
     nascimento:"2021/01/01",
     role:"admin",
@@ -9,15 +9,28 @@ const user = {
     }
 }
 
-user.exeibirInfos()
-
-const exibir = function() {
-    console.log(this.nome)
+const admin = {
+    nome: "Mariana",
+    email:"m@m.com",
+    role: "admin",
+    criarCurso() {
+        console.log('curso criado !')
+    }
 }
 
-const exibirNome = exibir.bind(user)
-exibirNome()
-exibir()
+Object.setPrototypeOf(admin, user)
+admin.criarCurso()
+admin.exeibirInfos()
+
+//user.exeibirInfos()
+
+//const exibir = function() {
+//    console.log(this.nome)
+//}
+
+//const exibirNome = exibir.bind(user)
+//exibirNome()
+//exibir()
 
 //const exibir = user.exeibirInfos
 //exibir()
