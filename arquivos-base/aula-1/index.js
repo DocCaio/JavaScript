eb31be4337c15c76024cbd656daa50d8f8c85aa7
Caio <108128119+DocCaio@users.noreply.github.com>
2023-07-10 09:08:00 -0300

    up

--- arquivos-base/aula-1/index.js
+++ arquivos-base/aula-1/index.js
@@ -9,11 +9,31 @@ function justaLista(lista1 , lista2) {
     while(posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length ) {
         let produtoAtualLista1 = lista1[posicaoAtualLista1];
         let produtoAtualLista2 = lista2[posicaoAtualLista2];
+        console.log(`comparando ${produtoAtualLista1.titulo} com ${produtoAtualLista2.titulo}`)
 
         if (produtoAtualLista1.preco < produtoAtualLista2.preco) {
-            listaFinal[atual] = 0;
+            listaFinal[atual] = produtoAtualLista1;
+            posicaoAtualLista1++;
+
+        }else {
+            listaFinal[atual] = produtoAtualLista2;
+            posicaoAtualLista2++;
+        }
+
+        while (posicaoAtualLista1 < lista1.length) {
+            listaFinal[atual] = lista1[posicaoAtualLista1];
+            posicaoAtualLista2++;
+            atual++;
+        }
+
+        while (posicaoAtualLista2 < lista2.length) {
+            listaFinal[atual] = lista1[posicaoAtualLista2];
+            posicaoAtualLista1++;
+            atual++;
         }
     }
 
     return listaFinal;
 }
+
+console.log(justaListas(edGalho , edFolha))
