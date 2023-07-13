c6f8f1658772fd81ea8d5cfe2277a5246efdc9e5
Caio <108128119+DocCaio@users.noreply.github.com>
2023-07-13 09:01:14 -0300

    up

--- node-lib/index.js
+++ node-lib/index.js
@@ -1,3 +1,20 @@
-console.log('olá mundo');
-console.log('São geralmente recuperados apartir de um objeto');
-console.log('São geralmente recuperados apartir de um objeto');
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro (erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivos no diretório'))
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+         if (erro) {
+            trataErro(erro);
+         }
+
+        console.log(chalk.green(texto));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
