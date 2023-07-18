5887ebb465c6d0868c58d7d392e9141e9f81b9b4
Caio <108128119+DocCaio@users.noreply.github.com>
2023-07-18 07:41:28 -0300

    up

--- node-lib/src/http-validacao.js
+++ node-lib/src/http-validacao.js
@@ -2,7 +2,20 @@ function extrairLinks (arrLinks) {
   return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
 
+async function checaStatus (listaURLs) {
+  return listaURLs.map(async (url) => {
+      const response = await fetch(url)
+      return response.status;
+  })
+}
 
 export default   function listaValidada (listaDeLinks) {
    return extrairLinks(listaDeLinks);
 }
+
+
+//const res = await fetch('https://nodejs.org/api/documentation.json');
+//if (res.ok) {
+ // const data = await res.json();
+ // console.log(data);
+//}
