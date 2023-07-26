c9e873e0299ed63d8842cbb1d770e04fef389871
Caio <108128119+DocCaio@users.noreply.github.com>
2023-07-26 13:59:38 -0300

    up

--- Robotron-2000/js/main.js
+++ Robotron-2000/js/main.js
@@ -1,1 +1,25 @@
-alert('oi');
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
+
+const controle = document.querySelectorAll(".controle-a-ajuste");
+
+controle.forEach ( (elemento) => {
+     elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+
+     })
+})
+
+
+
+
+
+function manipulaDados(operacao) {
+    if(operacao === "_") { 
+        braco.value = parseInt(braco.value) - 1
+    }else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}
+
