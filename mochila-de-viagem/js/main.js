40f8058185ff83c253a77104a257f4e9807d2254
Caio <108128119+DocCaio@users.noreply.github.com>
2023-07-31 09:05:46 -0300

    up

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,17 +1,37 @@
-const form = document.getElementById("novoItem");
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = []
 
-form.addEventListener("submit" ,(evento) => {
+form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    criaElemento(evento.target.elements['nome'].value, quantidade);
-  
-});
+
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
+})
 
 function criaElemento(nome, quantidade) {
-const novoItem = document.createElement('li');
-novoItem.classList.add("item")
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
 
-const numeroItem = document.createElement('strong')
-numeroItem.innerHTML = quantidade
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+    novoItem.appendChild(numeroItem)
 
-novoItem.innerHTML = numeroItem + nome
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
 }
