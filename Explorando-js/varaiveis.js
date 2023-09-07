3a4df31caaabac334adb628ca8955c517d447edb
Caio <108128119+DocCaio@users.noreply.github.com>
2023-09-07 09:27:48 -0300

    up

---
+++ Explorando-js/varaiveis.js
@@ -0,0 +1,9 @@
+//console.log("trabalhando com variaveis");
+console.log("Trabalhando com variáveis");
+//js é case sensitive
+
+const idade = 29 ;
+const nome = "Ricardo";
+
+// não fazer
+//mes = "Janeiro";
