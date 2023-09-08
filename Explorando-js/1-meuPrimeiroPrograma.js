861f9b74eff7968132ef2ef62f149afbabe58fd7
Caio <108128119+DocCaio@users.noreply.github.com>
2023-09-08 08:52:24 -0300

    up

--- Explorando-js/1-meuPrimeiroPrograma.js
+++ Explorando-js/1-meuPrimeiroPrograma.js
@@ -1,7 +1,8 @@
-console.log("Meu programa");
+console.log("Meu Primeiro programa: trabalhando com variáveis");
 
-const idade = 25;
+const idade = 26;
 
+console.log("idade");
 console.log(idade);
 console.log(idade+2);
 console.log(idade-2);
--- Explorando-js/varaiveis.js
+++
@@ -1,9 +0,0 @@
-//console.log("trabalhando com variaveis");
-console.log("Trabalhando com variáveis");
-//js é case sensitive
-
-const idade = 29 ;
-const nome = "Ricardo";
-
-// não fazer
-//mes = "Janeiro";
