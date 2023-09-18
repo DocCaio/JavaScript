3e0a618e722a60891a3ee8334ceead00fff20bfd
Caio <108128119+DocCaio@users.noreply.github.com>
2023-09-18 16:30:13 -0300

    up

--- bytebank-js/script/scripts.js
+++ bytebank-js/script/scripts.js
@@ -1,22 +1,17 @@
+import imprimeCotacao from "./imprimeCotacao.js";
 const graficoDolar = document.getElementById('graficoDolar');
 
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Dólar',
+            data: [],
             borderWidth: 1
         }]
     },
 });
-setInterval(() => conectaAPI(), 5000);
-async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);
-}
 
 function geraHorario() {
     let data = new Date();
@@ -25,4 +20,20 @@ function geraHorario() {
     return horario;
 }
 
-geraHorario();
+function adicionarDados(grafico, legenda, dados) {
+    grafico.data.labels.push(legenda);
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados);
+    })
+    grafico.update();
+}
+
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    imprimeCotacao("dolar", valor);
+    adicionarDados(graficoParaDolar, tempo, valor);
+})
