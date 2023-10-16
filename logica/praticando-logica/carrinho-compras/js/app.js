c5cedb02ed0f73482f241a0682b16bb057435bf5
Caio <108128119+DocCaio@users.noreply.github.com>
2023-10-16 13:07:57 -0300

    up

--- logica/praticando-logica/carrinho-compras/js/app.js
+++ logica/praticando-logica/carrinho-compras/js/app.js
@@ -1,15 +1,14 @@
 function adicionar() {
-    //recuperar valores nome do produto, quantidade e valor 
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
+    //recuperar valores nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$ ')[1]; 
+    let quantidade = document.getElementById('quantidade'); 
+    alert(nomeProduto);
+    alert(valorUnitario);
+    alert(quantidade.value);
+    let preco = quantidade.value * valorUnitario;
+    alert(preco);
+    
+}
 
-    //calcular o preço, o nosso subtotal 
-    //adicionar no carrinho 
-    //atualizar o valor total
-    
-    }
-    
-    function limpar() {
-    
-    }
-    
