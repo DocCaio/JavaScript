ac683affa7dee139514e1dd10e874ccc34edaec2
Caio <108128119+DocCaio@users.noreply.github.com>
2023-11-29 10:22:37 -0300

    up

--- AmazanamentoDados/fokus-local-storage-main/script-crud.js
+++ AmazanamentoDados/fokus-local-storage-main/script-crud.js
@@ -1,50 +1,61 @@
-const btnAdiconarTarefa = document.querySelector('.app__button--add-task');
-const formAdicionarTarefa = document.querySelector('.app__form-add-task hidden');
-const textArea = document.querySelector('.app__form-textarea');
+// encontrar o botão adicionar tarefa
 
-const tarefas = [];
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-textarea')
+const ulTarefas = document.querySelector('.app__section-task-list')
 
-function criarElementoTrefa(tarefa) {
-    const li = document.createElement('li');
-    li.classList.add('app__section-task-list-item');
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li')
+    li.classList.add('app__section-task-list-item')
 
     const svg = document.createElement('svg')
     svg.innerHTML = `
-    <svg>
-       <svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-       </svg>
-    </svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+                fill="#01080E"></path>
+        </svg>
     `
-    const paragrafo = document.createElement('p');
+    const paragrafo = document.createElement('p')
     paragrafo.textContent = tarefa.descricao
+    paragrafo.classList.add('app__section-task-list-item-description')
 
-    const botao = document.createElement('button');
-    const imagemBotao = document.createElement('img');
+    const botao = document.createElement('button')
+    botao.classList.add('app_button-edit')
 
-    imagemBotao.setAttribute('src', '/imagens/edit.png');
-    botao.append(imagemBotao);
+    const imagemBotao = document.createElement('img')
+    imagemBotao.setAttribute('src', '/imagens/edit.png')
+    botao.append(imagemBotao)
 
-    li.append(svg);
-    li.append(paragrafo);
-    li.append(botao);
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
 
-   
+    return li
+}   
 
-}
-
-btnAdiconarTarefa.addEventListener('click', () => {
-    formAdicionarTarefa.classList.toggle('hidden');
-
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
 })
 
 formAdicionarTarefa.addEventListener('submit', (evento) => {
-    evento.preventDefault();    
+    evento.preventDefault();
     const tarefa = {
-        descricao: textArea.value
+        descricao: textarea.value
     }
-    tarefas.push(tarefa);
-    localStorage.setItem('tarefas' , JSON.stringify(tarefas));
-
+    tarefas.push(tarefa)
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    textarea.value = ''
+    formAdicionarTarefa.classList.add('hidden')
 })
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+});
