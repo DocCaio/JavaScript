47884ee15e336ac0d896f0fcf76ec39dd4dfc97d
Caio <108128119+DocCaio@users.noreply.github.com>
2024-01-18 08:56:13 -0300

    up

---
+++ variaveis-funcoes/exercicios/ex3.js
@@ -0,0 +1,12 @@
+//Declare duas variáveis, uma contendo um 
+//número e outra contendo uma string. 
+//Em seguida, combine-as em uma terceira 
+//variável usando template strings para
+ //montar uma frasee exiba o resultado
+ // no console.
+
+const numero = 42;
+const string = 'universo';
+
+const texto = `${numero} é a resposta para a pergunta mais importante do ${universo}`;
+console.log(texto);
