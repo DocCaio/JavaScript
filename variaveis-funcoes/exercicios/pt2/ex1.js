808f7b155fabfbaa48d512733616148b384417b2
Caio <108128119+DocCaio@users.noreply.github.com>
2024-01-19 09:58:28 -0300

    up

---
+++ variaveis-funcoes/exercicios/pt2/ex1.js
@@ -0,0 +1,8 @@
+//Crie uma variável contendo uma
+ //frase. Em seguida, exiba no console o
+ //comprimento da frase e a mesma frase 
+  //em constras maiúsculas.
+
+const frase = 'Esta é uma frase de exemplo.';
+console.log(frase.length); // Comprimento da frase
+console.log(frase.toUpperCase()); // Frase em constras maiúsculas
---
+++ variaveis-funcoes/exercicios/pt2/ex2.js
@@ -0,0 +1,10 @@
+//Declare duas variáveis, uma inicializada 
+//com valor null e outra que não recebe
+ //atribuição (resultando em undefined). 
+//Exiba os valores no console.
+
+const valorNull = null;
+let valorUndefined;
+
+console.log('Valor de valorNull:', valorNull); // Saída: null
+console.log('Valor de valorUndefined:', valorUndefined); // Saída: undefined
