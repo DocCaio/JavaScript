2176637759fb26f253e1c58ded9388d728c2f6cf
Caio <108128119+DocCaio@users.noreply.github.com>
2024-01-25 09:41:56 -0300

    up

---
+++ arrays/exercicios/pt2/ex6.js
@@ -0,0 +1,4 @@
+// e imprima o elemento na segunda linha e terceira coluna da 
+//lista bidimensional matriz criada no exercício anterior.
+
+console.log('Elemento na segunda linha e terceira coluna:', matriz[1][2]);
