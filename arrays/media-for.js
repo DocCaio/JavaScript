5da98671d4b3003ad9581cf254d92f688e3afe80
Caio <108128119+DocCaio@users.noreply.github.com>
2024-01-26 10:16:46 -0300

    up

--- arrays/media-for.js
+++ arrays/media-for.js
@@ -1,14 +1,10 @@
-//indice é a mesma coisa que i
 const notas = [10, 6.5, 8, 7.5];
 
 let somaDasNotas = 0;
 
-
 for (let i = 0; i < notas.length; i++) {
-    somaDasNotas += notas [i];
-
+  somaDasNotas += notas[i];
 }
 
 const media = somaDasNotas / notas.length;
-
-console.log(`A media das notas é ${media}`);
+console.log(`a média das notas é ${media}`);
