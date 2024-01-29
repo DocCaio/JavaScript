732d982c84b9989ecd7d6389bb543c61474fb78d
Caio <108128119+DocCaio@users.noreply.github.com>
2024-01-29 13:49:02 -0300

    up

---
+++ arrays/exercicios/pt4/ex1.js
@@ -0,0 +1,8 @@
+//Utilize o método forEach para imprimir cada elemento de um array juntamente com seu índice.
+
+
+const meuArray = ['a', 'b', 'c', 'd', 'e'];
+
+meuArray.forEach((elemento, indice) => {
+  console.log(`Índice: ${indice}, Valor: ${elemento}`);
+});
